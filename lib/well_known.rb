5a15db298ce0a351af70dbb5e4eaf463f088b9b9
() <nilclass@riseup.net>
2012-03-02 21:15:34 +0000

    a bit of cleanup

--- lib/well_known.rb
+++ lib/well_known.rb
@@ -4,29 +4,32 @@ require 'sinatra'
 require 'nokogiri'
 
 class WellKnown < Sinatra::Base
-  #HOST = 'heahdk.net'
+
   HOST = 'localhost'
-  PORT = '3000'
+  PORT = 9292
   SCHEME = 'http'
 
-=begin
-
-<?xml version='1.0' encoding='UTF-8'?>
-<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'
-     xmlns:hm='http://host-meta.net/xrd/1.0'>
-
-    <hm:Host>example.com</hm:Host>
-
-    <Link rel='lrdd'
-          template='http://example.com/describe?uri={uri}'>
-        <Title>Resource Descriptor</Title>
-    </Link>
-</XRD>
-=end
+  ## Set PREFIX to '', if you want to proxy_pass /.well-known
+  ## to this app from a frontend server (which is recommended)
+  PREFIX = '/.well-known'
 
   get '/host-meta' do
+
     allow_origin('*')
     content_type 'application/xrd+xml'
+
+    # <?xml version='1.0' encoding='UTF-8'?>
+    # <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'
+    #      xmlns:hm='http://host-meta.net/xrd/1.0'>
+    #
+    #     <hm:Host>example.com</hm:Host>
+    #
+    #     <Link rel='lrdd'
+    #           template='http://example.com/describe?uri={uri}'>
+    #         <Title>Resource Descriptor</Title>
+    #     </Link>
+    # </XRD>
+
     Nokogiri::XML::Builder.new do |xml|
       xml.XRD(
         'xmlns' => 'http://docs.oasis-open.org/ns/xri/xrd-1.0',
@@ -44,52 +47,82 @@ class WellKnown < Sinatra::Base
     end.to_xml
   end
 
-=begin
-<?xml version='1.0' encoding='UTF-8'?>
-<XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
-
-    <Subject>acct:joe@example.com</Subject>
-    <Alias>http://example.com/profiles/joe</Alias>
-
-    <Link rel='http://portablecontacts.net/spec/1.0'
-          href='http://example.com/api/people/' />
-    <Link rel='http://webfinger.net/rel/profile-page'
-          type='text/html'
-          href='http://example.com/profiles/joe' />
-    <Link rel='http://microformats.org/profile/hcard'
-          type='text/html'
-          href='http://example.com/profiles/joe' />
-    <Link rel='describedby'
-          type='text/html'
-          href='http://example.com/profiles/joe' />
-    <Link rel='http://webfinger.net/rel/avatar'
-          href='http://example.com/profiles/joe/photo' />
-</XRD>
-=end
-
   get '/lrdd/describe' do
+
     allow_origin('*')
     content_type 'application/xrd+xml'
 
-    uri_string = params[:uri]
-    throw :precondition_not_met unless uri_string
+    unless uri_string = params[:uri]
+      throw :halt, [412, "Precondition Failed\n"]
+    end
 
     uri = URI.parse(uri_string)
-    throw :not_allowed unless uri.host == HOST
 
-    uri.scheme = 'acct'
+    unless uri.host == HOST
+      throw :halt, [404, "Not Found\n"]
+    end
 
-    Nokogiri::XML::Builder.new do |xml|
-      xml.XRD(
-        'xmlns' => 'http://docs.oasis-open.org/ns/xri/xrd-1.0'
-      ) do
-        xml.Subject(uri.to_s)
-        xml.Alias(profile_url(uri.user))
-      end
-    end.to_xml
+    # <?xml version='1.0' encoding='UTF-8'?>
+    # <XRD xmlns='http://docs.oasis-open.org/ns/xri/xrd-1.0'>
+    #
+    #     <Subject>acct:joe@example.com</Subject>
+    #     <Alias>http://example.com/profiles/joe</Alias>
+    #
+    #     <Link rel='http://portablecontacts.net/spec/1.0'
+    #           href='http://example.com/api/people/' />
+    #     <Link rel='http://webfinger.net/rel/profile-page'
+    #           type='text/html'
+    #           href='http://example.com/profiles/joe' />
+    #     <Link rel='http://microformats.org/profile/hcard'
+    #           type='text/html'
+    #           href='http://example.com/profiles/joe' />
+    #     <Link rel='describedby'
+    #           type='text/html'
+    #           href='http://example.com/profiles/joe' />
+    #     <Link rel='http://webfinger.net/rel/avatar'
+    #           href='http://example.com/profiles/joe/photo' />
+    # </XRD>
+
+
+    xrd do |x|
+      x.Subject(uri.to_s)
+      x.Alias(profile_url(uri.user))
+    end
   end
 
   helpers do
+
+    ## Headers
+
+    def allow_origin(origin)
+      headers('Access-Control-Allow-Origin' => origin)
+    end
+
+    XRD_NS = {
+      :hm => 'http://host-meta.net/xrd/1.0'
+    }
+
+    ## XRD
+    def xrd(*ns)
+      namespaces = {
+        'xmlns' => 'http://docs.oasis-open.org/ns/xri/xrd-1.0'
+      }.merge(
+        Hash[
+          *ns.map {|n|
+            ["xmlns:#{n}", XRD_NS[n]]
+          }
+        ]
+      )
+
+      Nokogiri::XML::Builder.new do |xml|
+        xml.XRD(namespaces) do
+          yield(xml)
+        end.to_xml
+      end
+    end
+
+    ## Urls
+
     def lrdd_describe_url
       well_known_url('lrdd', 'describe?uri={uri}')
     end
@@ -103,12 +136,9 @@ class WellKnown < Sinatra::Base
     end
 
     def base_url
-      port = [80, 443].include?(PORT) ? '' : ":#{PORT}"
+      port = [80, 443].include?(PORT.to_i) ? '' : ":#{PORT}"
       "#{SCHEME}://#{HOST}#{port}"
     end
 
-    def allow_origin(origin)
-      headers('Access-Control-Allow-Origin' => origin)
-    end
   end
 end
